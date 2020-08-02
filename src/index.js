b8a656ab800e577a56b37382dafde5a5a4d7944f
Egda Barrios <egdabarrios@gmail.com>
2020-08-02 10:15:04 -0500

    version final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,90 @@
+/*Acá escribirás todo el código que tenga que ver con la interacción del DOM
+(seleccionar, actualizar y manipular elementos del DOM y eventos).
+Es decir, en este archivo deberás invocar las funciones `isValid` y `maskify`
+según sea necesario para actualizar el resultado en la pantalla (UI).*/
+
+
 import validator from './validator.js';
+const myBtn = document.getElementById("myBtn");
+const response1 = document.getElementById("message1");
+const response2 = document.getElementById("message2");
+const response3 = document.getElementById("message3");
+
+function validation() {
+// console.log('Hola mundo!');
+// variable de campo de usuario
+const cardNumber = document.getElementById("cardNumber").value;
+
+
+if (cardNumber === ""){
+  response1.innerHTML = '<p>*Ingrese el número de tarjeta</p>';
+  
+  document.getElementById("message1").style.display = "block";
+  document.getElementById("message2").style.display = "none";
+  document.getElementById("message3").style.display = "none";
+  
+}
+
+else if ((!/\d{15,16}(~\W[a-zA-Z])*$/g.test(cardNumber)) || (cardNumber.length >16)){
+    response3.innerHTML = '<p>Tarjeta inválida, verifique</p>';    
+    document.getElementById("message1").style.display = "none";
+    document.getElementById("message2").style.display = "none";
+    document.getElementById("message3").style.display = "block";
+    return false;
+}
+
+// Dígito entre 15 y 16 de longitud; sin símbolos ni letras
+if ((/\d{15,16}(~\W[a-zA-Z])*$/g.test(cardNumber)) ){
+  
+  const maskifyNumber = validator.maskify(cardNumber);
+  const isValidCard = validator.isValid(cardNumber);
+  
+  document.getElementById("cardNumber").value=`${maskifyNumber}`;
+  
+  if (isValidCard === true) {
+    response2.innerHTML = '<p>Tarjeta válida, continúe</p>';
+    document.getElementById("message1").style.display = "none";
+    document.getElementById("message2").style.display = "block";
+    document.getElementById("message3").style.display = "none";
+    
+  } 
+  else {
+    response3.innerHTML = '<p>Tarjeta inválida, verifique</p>';    
+    document.getElementById("message1").style.display = "none";
+    document.getElementById("message2").style.display = "none";
+    document.getElementById("message3").style.display = "block";
+    
+  } 
+  
+}
+
+    
+    
+  
+  }
+
+
+myBtn.addEventListener("click", validation);
+
 
 console.log(validator);
+
+// 1.- Enumerar las funciones.
+// 2.- Agregar funcionalidad al botón.
+// 3.- Asignar variables.
+// 4.- Establecer condiciones.
+
+/*4417123456789113
+Test Credit Card Numbers
+American Express: 378282246310005
+American Express: 371449635398431
+Discover: 6011111111111117
+Discover: 6011000990139424
+JCB: 3530111333300000
+JCB: 3566002020360505
+Mastercard: 5555555555554444
+Mastercard: 5105105105105100
+Visa: 4111111111111111*/
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,94 @@
-const validator = {
-  // ...
-};
+/*Acá escribirás las funciones necesarias para que el usuario pueda verificar la
+tarjeta de crédito y ocultar los dígitos de su número de tarjeta.
+Esta función debe ser pura e independiente del DOM.
 
-export default validator;
+Para esto debes implementar el **objeto `validator`**, el cual ya se encuentra
+_exportado_ en el _boilerplate_. Este objeto (`validator`) contiene
+dos métodos (`isValid` y `maskify`):
+
+* **`validator.isValid(creditCardNumber)`**: `creditCardNumber` es un `string`
+con el número de tarjeta que se va a verificar. Esta función debe retornar un
+`boolean` dependiendo si es válida de acuerdo al [algoritmo de Luhn](https://es.wikipedia.org/wiki/Algoritmo_de_Luhn).
+
+* **`validator.maskify(creditCardNumber)`**: `creditCardNumber` es un `string` con
+el número de tarjeta y esta función debe retornar un `string` donde todos menos
+los últimos cuatro caracteres sean reemplazados por un numeral (`#`) o 🐱.
+Esta función deberá siempre mantener los últimos cuatro caracteres intactos, aún
+cuando el `string` sea de menor longitud.
+
+Ejemplo de uso
+
+    ```js
+    maskify('4556364607935616') === '############5616'
+    maskify(     '64607935616') ===      '#######5616'
+    maskify(               '1') ===                '1'
+    maskify(               '')  ===                ''
+    ```*/
+
+    const validator = {
+      isValid:(cardNumber) => {
+        let credicardNumArray = cardNumber.split("");
+        let sum = 0;
+        let singleNums = [];
+        let doubleNums = [];
+        let finalArray = undefined;
+        let validCard = false;
+              
+        /*if(credicardNumArray.length === 15){  //american express
+           for(var i = credicardNumArray.length-1; i>=0; i--){
+              if(i % 2 === 0){
+                 singleNums.push(credicardNumArray[i]);
+              }else{
+                 doubleNums.push((credicardNumArray[i] * 2).toString());
+              }
+           }
+        }*/ if(credicardNumArray.length === 16){ //else if
+           for(var i = credicardNumArray.length-1; i>=0; i--){
+              if(i % 2 !== 0){
+                 singleNums.push(credicardNumArray[i]);
+              }else{
+                 doubleNums.push((credicardNumArray[i] * 2).toString());
+              }
+           }
+        }
+        //joining makes an array to a string, then split them up again
+        //so that every number is a single digit and convert back to array
+    
+        doubleNums = doubleNums.join("").split("");
+        finalArray = doubleNums.concat(singleNums);
+    
+        for(var j = 0; j<finalArray.length; j++){
+           sum += parseInt(finalArray[j]);
+        }
+    
+        if(sum % 10 === 0){
+           validCard = true;
+           
+        }
+        else{
+           validCard = false;
+           
+        }
+                
+        return validCard;
+      },
+    
+      maskify:(cardNumber) => {
+        
+        let lengthCardNumber = cardNumber.length;
+        let stringResult = "";
+              
+        for(var i = 0; i < lengthCardNumber - 4; i++){
+        stringResult += "#";  
+      }
+        return stringResult + cardNumber.substring(lengthCardNumber-4, lengthCardNumber);
+      }
+      } 
+             
+    export default validator;
+      
+    
+
+    // 1.- Definir los dos métodos: isValid y maskify.
+    // 2.- Escribir las funciones respectivas de acuerdo a las especificaciones solicitadas.
+  
